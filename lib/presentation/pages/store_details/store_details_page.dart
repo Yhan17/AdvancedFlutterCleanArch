5354ae57e3df5d1d1e8ec6a4a1de74db2188e752
Yhan17 <yhannunesleal@gmail.com>
2022-01-14 17:17:10 -0300

    🎊Add Course Route Managemenent

---
+++ lib/presentation/pages/store_details/store_details_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class StoreDetailsPage extends StatefulWidget {
+  const StoreDetailsPage({Key? key}) : super(key: key);
+
+  @override
+  _StoreDetailsPageState createState() => _StoreDetailsPageState();
+}
+
+class _StoreDetailsPageState extends State<StoreDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
